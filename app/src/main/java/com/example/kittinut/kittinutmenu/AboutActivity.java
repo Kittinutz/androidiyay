62d44687362af1abca7fc76ce1e758508d8d7f31
knightza123 <knightza123@hotmail.com>
2018-03-30 02:37:51 +0700

    first commit

---
+++ app/src/main/java/com/example/kittinut/kittinutmenu/AboutActivity.java
@@ -0,0 +1,8 @@
+package com.example.kittinut.kittinutmenu;
+
+/**
+ * Created by kittinut on 30/3/2018 AD.
+ */
+
+public class AboutActivity {
+}
---
+++ app/src/main/java/com/example/kittinut/kittinutmenu/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.kittinut.kittinutmenu;
+
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mDrawerTitle = {"Home", "Profile", "Setting", "Contact"};
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private ListView mListView;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
+                mDrawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mListView = findViewById(R.id.drawer);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerTitle);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) mListView.getItemAtPosition(position);
+
+                mDrawerLayout.closeDrawers();
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + position + "  ListItem : " + itemValue, Toast.LENGTH_SHORT)
+                        .show();
+                switch (itemValue) {
+                    case "Profile":
+                        setContentView(R.layout.profile_main);
+                    case "Home":
+                        setContentView(R.layout.activity_main);
+                    case "Setting":
+                        setContentView(R.layout.setting_main);
+                    case "Contact":
+                        setContentView(R.layout.contact_main);
+
+                }
+            }
+        });
+
+
+        // ========== Custom list view =======
+        int[] resId = {R.drawable.one, R.drawable.two, R.drawable.three};
+
+        String[] list = {"Naruto", "Ironman", "Yippee Ki Yay "};
+
+
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), list, resId);
+
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long row_id) {
+                TextView textView = (TextView) view.findViewById(R.id.textView1);
+                System.out.println(textView.getText());
+                Toast.makeText(getApplicationContext(), "Clicked on item:" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        switch (item.getItemId()) {
+            case R.id.Login:
+                Toast.makeText(this, "New!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Help:
+                Toast.makeText(this, "Help!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
